c0e0b826a351015a73e65178a3fd044cacd8082e
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-06-11 00:16:43 +0200

    feat: minimal working example

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>@nuxtjs/strapi payload extraction test</h1>
+    <p>Data from the "test" collection should be prerendered below</p>
+    <code>{{ data }}</code>
+  </div>
+</template>
+
+<script setup lang="ts">
+// this breaks on `npm run generate`
+const strapi = useStrapi()
+const data = await strapi.findOne('test');
+
+// this useFetch example works as expected
+// const strapiUrl = useStrapiUrl()
+// const data = await useFetch(strapiUrl + '/test');
+</script>
